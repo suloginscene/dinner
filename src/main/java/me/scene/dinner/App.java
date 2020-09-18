b312396abd94cb17a039bd6dbd199e2e3372ce86
suloginscene <suloginscene@gmail.com>
2020-09-18 10:18:27 +0900

    [#1] configure properties and logging

--- src/main/java/me/scene/dinner/App.java
+++ src/main/java/me/scene/dinner/App.java
@@ -1,13 +1,20 @@
 package me.scene.dinner;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+    public static final String SPRING_CONFIGS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "file:/dinner-ref/app/config/file-application.yml";
 
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(App.class)
+                .properties(SPRING_CONFIGS)
+                .run(args);
+    }
 }
