0e81593fd9e798d63775e43354aae5a872ac8840
suloginscene <suloginscene@gmail.com>
2020-09-18 14:05:55 +0900

    [#1] automate deployment

---
+++ src/main/java/me/scene/dinner/HomeController.java
@@ -0,0 +1,12 @@
+package me.scene.dinner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
