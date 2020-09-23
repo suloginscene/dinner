1cad25115266333702f585becb61d60c2af81949
suloginscene <suloginscene@gmail.com>
2020-09-23 23:06:04 +0900

    [#2] design draft of overlay navigation

---
+++ src/main/resources/static/js/nav/accordion.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+
+    const acc = document.getElementsByClassName("accordion");
+
+    for (let i = 0; i < acc.length; i++) {
+
+        acc[i].addEventListener("click", function () {
+            const chevron = acc[i].lastElementChild;
+
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+            chevron.classList.toggle("fa-flip-vertical");
+
+            /* Toggle between hiding and showing the active panel */
+            let panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+
+    }
+
+}
