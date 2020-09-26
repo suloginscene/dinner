5385c26267aee59520effd8b5d6e8d8ec5c0c903
suloginscene <suloginscene@gmail.com>
2020-09-27 07:42:58 +0900

    [#2] prototype of individual topic page

---
+++ src/main/java/me/scene/dinner/domain/topic/TopicController.java
@@ -0,0 +1,19 @@
+package me.scene.dinner.domain.topic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class TopicController {
+
+    public static final String URL = "/topics";
+
+    @GetMapping(URL + "/{title}")
+    public String readAnArticle(@PathVariable String title, Model model) {
+        model.addAttribute("title", title);
+        return "page/topic/view";
+    }
+
+}
--- src/main/java/me/scene/dinner/infra/config/SecurityConfig.java
+++ src/main/java/me/scene/dinner/infra/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package me.scene.dinner.infra.config;
 import me.scene.dinner.MainController;
 import me.scene.dinner.domain.account.AccountController;
 import me.scene.dinner.domain.article.ArticleController;
+import me.scene.dinner.domain.topic.TopicController;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(HttpMethod.GET,
                         MainController.URL_HOME, MainController.URL_ABOUT,
                         AccountController.URL_SIGNUP, AccountController.URL_LOGIN,
-                        ArticleController.URL+"/*"
+                        ArticleController.URL + "/*", TopicController.URL + "/*"
                 ).permitAll()
 
 //                .mvcMatchers(HttpMethod.POST,
