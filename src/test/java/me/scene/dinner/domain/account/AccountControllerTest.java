a6d56a45cc83acdefb7af43639f1e3df0eedf8eb
suloginscene <suloginscene@gmail.com>
2020-09-28 14:38:47 +0900

    [#3] initial commit of account

---
+++ src/test/java/me/scene/dinner/domain/account/AccountControllerTest.java
@@ -0,0 +1,28 @@
+package me.scene.dinner.domain.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired MockMvc mockMvc;
+
+    @Test
+    void signup_ok() throws Exception {
+        mockMvc.perform(
+                get(AccountController.URL_SIGNUP)
+        )
+                .andExpect(status().isOk())
+        ;
+    }
+
+}
