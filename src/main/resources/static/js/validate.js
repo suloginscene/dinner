57b8ed9342c906152b4549bb46361c346913e021
suloginscene <suloginscene@gmail.com>
2020-10-21 02:34:15 +0900

    [#2] validate signup-form in javascript

--- src/main/resources/static/js/validate.js
+++ src/main/resources/static/js/validate.js
@@ -1,15 +1,34 @@
-const validate = function () {
-    const forms = document.getElementsByClassName('needs-validation');
+function validate() {
 
-    Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener('submit', function (event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated')
-        }, false)
-    })
+    const form = document.getElementById('signup-form');
+
+    const checkStrings = function (e) {
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        form.classList.add('was-validated');
+    };
+
+
+    const term = document.getElementById('term');
+
+    const checkTerm = function (e) {
+        const termHint = document.getElementById('term-hint');
+        if (!term.checked) {
+            e.preventDefault();
+            termHint.style.display = 'block';
+        } else {
+            termHint.style.display = 'none';
+        }
+    }
+
+
+    form.addEventListener('input', checkStrings, false);
+    term.addEventListener('input', checkTerm, false);
+
+    form.addEventListener('submit', checkStrings, false);
+    form.addEventListener('submit', checkTerm, false);
 
 }
 
