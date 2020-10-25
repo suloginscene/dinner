c885453be7c69cd2733ae1ce71df9eb9ac04796b
suloginscene <suloginscene@gmail.com>
2020-10-25 23:55:21 +0900

    [#4] map hierarchy

--- src/main/java/me/scene/dinner/domain/board/article/Article.java
+++ src/main/java/me/scene/dinner/domain/board/article/Article.java
@@ -2,10 +2,12 @@ package me.scene.dinner.domain.board.article;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import me.scene.dinner.domain.board.topic.Topic;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter @EqualsAndHashCode(of = "id")
@@ -15,6 +17,9 @@ public class Article {
     private Long id;
 
     private Long writer;
+
+    @ManyToOne
+    private Topic topic;
 
     private String title;
 
--- src/main/java/me/scene/dinner/domain/board/topic/Topic.java
+++ src/main/java/me/scene/dinner/domain/board/topic/Topic.java
@@ -2,10 +2,12 @@ package me.scene.dinner.domain.board.topic;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import me.scene.dinner.domain.board.magazine.Magazine;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter @EqualsAndHashCode(of = "id")
@@ -13,6 +15,9 @@ public class Topic {
 
     @Id @GeneratedValue
     private Long id;
+
+    @ManyToOne
+    private Magazine magazine;
 
     private String title;
 
