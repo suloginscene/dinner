f938f157488784dcb785e25d233099d656a66a01
suloginscene <suloginscene@gmail.com>
2020-11-14 23:53:21 +0900

    [#4] refactor: Javascript validation
    Extract form-validation method from signup-validation code to prepare
    board-validation.

---
+++ src/main/resources/static/js/lib/validate.js
@@ -0,0 +1,17 @@
+const validate = function (form) {
+
+    const $form = $(form);
+
+    const checkStrings = function (e) {
+        if ($form[0].checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        $form.addClass('was-validated');
+    };
+
+
+    $form.bind('input', checkStrings);
+    $form.bind('submit', checkStrings);
+
+};
---
+++ src/main/resources/static/js/page/valid_signup.js
@@ -0,0 +1,26 @@
+const validateTerm = function () {
+
+    const form = document.getElementById('signup-form');
+    const term = document.getElementById('term');
+    const termHint = document.getElementById('term-hint');
+
+    const checkTerm = function (e) {
+        if (!term.checked) {
+            e.preventDefault();
+            termHint.style.display = 'block';
+        } else {
+            termHint.style.display = 'none';
+        }
+    };
+
+
+    form.addEventListener('submit', checkTerm, false);
+    term.addEventListener('input', checkTerm, false);
+
+};
+
+
+window.addEventListener('load', function () {
+    validate('#signup-form');
+    validateTerm();
+});
